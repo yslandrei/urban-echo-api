2ec63100f8fdfe93b0737b6e3f6c1428bf10bcc4
DomnicaTeodor <119111488+DomnicaTeodor@users.noreply.github.com>
2025-03-04 12:30:33 +0200

    another try at deployment

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import authRoutes from './routes/auth.routes';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(express.json());
